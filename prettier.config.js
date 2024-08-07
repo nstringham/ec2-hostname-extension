378b5404574922dd5f51026fcbeb8979456dfeb4
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-08-07 15:21:24 -0400

    build: ➕ install prettier-plugin-svelte

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
   printWidth: 120,
+  plugins: ["prettier-plugin-svelte"],
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
 };
